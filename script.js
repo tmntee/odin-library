f9ded91f2d1a0bc1ea5e992d7c0e2d1715b5ed72
tmntee <107428229+tmntee@users.noreply.github.com>
2024-05-24 13:23:44 -0400

    Add createHTMLcard function to Book prototype to abstractify gallery display process

--- script.js
+++ script.js
@@ -9,6 +9,49 @@ function Book(title, author, amntOfPages, pagesRead, haveRead, favorite) {
     this.favorited = favorite;
 }
 
+Book.prototype.createHTMLBookCard = function() {
+    let bookCard = document.createElement("div");
+    bookCard.setAttribute("class", "book-card");
+
+    let bookCover = document.createElement("img");
+    bookCover.setAttribute("class", "book-cover");
+    bookCover.setAttribute("src", "images/book_icon.svg");
+    bookCard.appendChild(bookCover);
+
+    let bookInfo = document.createElement("div");
+    bookInfo.setAttribute("class","book-info");
+
+    let bookTitle = document.createElement("p");
+    bookTitle.setAttribute("class", "title");
+    bookTitle.textContent = this.title;
+    bookInfo.appendChild(bookTitle);
+
+    let bookAuthor = document.createElement("p");
+    bookAuthor.setAttribute("class", "author");
+    bookAuthor.textContent = this.author;
+    bookInfo.appendChild(bookAuthor);
+
+    let bookPages = document.createElement("p");
+    bookPages.setAttribute("class", "pages");
+    bookPages.textContent = this.pages;
+    bookInfo.appendChild(bookPages);
+
+    bookCard.appendChild(bookInfo);
+
+    let bookNav = document.createElement("div");
+    bookNav.setAttribute("class", "book-nav");
+    
+    let buttonTexts = ["Remove", "Favorite", "Read"];
+    buttonTexts.forEach(function(text) {   
+        let button = document.createElement("button");
+        button.textContent = text;
+        bookNav.appendChild(button);
+    })
+    bookCard.appendChild(bookNav);
+
+    return bookCard;
+}
+
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
@@ -18,33 +61,7 @@ const bookGallery = document.querySelector(".book-gallery");
 function displayLibraryToBookGallery() {
     for (let i = 0; i < myLibrary.length; i++)
     {   
-        let bookCard = document.createElement("div");
-        bookCard.setAttribute("class", "book-card");
-
-        let bookCover = document.createElement("img");
-        bookCover.setAttribute("class", "book-cover");
-        bookCover.setAttribute("src", "images/book_icon.svg");
-        bookCard.appendChild(bookCover);
-
-        let bookInfo = document.createElement("div");
-        bookInfo.setAttribute("class","book-info");
-
-        let bookTitle = document.createElement("p");
-        bookTitle.setAttribute("class", "title");
-        bookTitle.textContent = myLibrary.at(i)["title"];
-        bookInfo.appendChild(bookTitle);
-
-        let bookAuthor = document.createElement("p");
-        bookAuthor.setAttribute("class", "author");
-        bookAuthor.textContent = myLibrary.at(i)["author"];
-        bookInfo.appendChild(bookAuthor);
-
-        let bookPages = document.createElement("p");
-        bookPages.setAttribute("class", "pages");
-        bookPages.textContent = myLibrary.at(i)["pages"];
-        bookInfo.appendChild(bookPages);
-
-        bookCard.appendChild(bookInfo);
+        let bookCard = myLibrary.at(i).createHTMLBookCard();
 
         bookGallery.appendChild(bookCard);
     }
