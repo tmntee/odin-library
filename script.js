ebba06e7bbd19a8eb99cd7dc73c48b5d33643f9b
tmntee <107428229+tmntee@users.noreply.github.com>
2024-05-24 21:19:41 -0400

    Rearrange elements in bookcard HTML and add styling to book cards excluding buttons

--- script.js
+++ script.js
@@ -1,117 +1,3 @@
-const myLibrary = [];
-
-function Book(title, author, amntOfPages, pagesRead, haveRead, favorite) {
-    this.title = title;
-    this.author = author;
-    this.pages = amntOfPages;
-    this.pagesRead = pagesRead;
-    this.haveRead = haveRead;
-    this.favorited = favorite;
-}
-
-Book.prototype.createHTMLBookCard = function() {
-    let bookCard = document.createElement("div");
-    bookCard.setAttribute("class", "book-card");
-
-    let bookCover = document.createElement("img");
-    bookCover.setAttribute("class", "book-cover");
-    bookCover.setAttribute("src", "images/book_icon.svg");
-    bookCard.appendChild(bookCover);
-
-    let bookInfo = document.createElement("div");
-    bookInfo.setAttribute("class","book-info");
-
-    let bookTitle = document.createElement("p");
-    bookTitle.setAttribute("class", "title");
-    bookTitle.textContent = this.title;
-    bookInfo.appendChild(bookTitle);
-
-    let bookAuthor = document.createElement("p");
-    bookAuthor.setAttribute("class", "author");
-    bookAuthor.textContent = this.author;
-    bookInfo.appendChild(bookAuthor);
-
-    let pagesSection = document.createElement("div");
-    pagesSection.setAttribute("class", "pages-section");
-
-    let bookPagesRead = document.createElement("p");
-    bookPagesRead.setAttribute("class", "pages-read");
-    bookPagesRead.textContent = this.pagesRead;
-    pagesSection.appendChild(bookPagesRead);
-
-    let division = document.createElement("p");
-    division.textContent = " / ";
-    pagesSection.appendChild(division);
-
-    let bookPages = document.createElement("p");
-    bookPages.setAttribute("class", "pages");
-    bookPages.textContent = this.pages;
-    pagesSection.appendChild(bookPages);
-
-    bookInfo.appendChild(pagesSection);
-
-    let subPageButton = document.createElement("button");
-    subPageButton.setAttribute("class", "page-button subtract");
-    subPageButton.textContent = "-";
-
-    subPageButton.addEventListener('click', () => {
-        if (this.pagesRead != 0)
-        {
-            this.pagesRead--;
-            bookPagesRead.textContent = this.pagesRead;
-        }
-    })
-
-    let addPageButton = document.createElement("button");
-    addPageButton.setAttribute("class", "page-button add");
-    addPageButton.textContent = "+";
-
-    addPageButton.addEventListener('click', () => {
-        if (this.pagesRead != this.pages)
-            {
-                this.pagesRead++;
-                bookPagesRead.textContent = this.pagesRead;
-            }
-    })
-
-    bookInfo.appendChild(subPageButton);
-    bookInfo.appendChild(addPageButton);
-
-    bookCard.appendChild(bookInfo);
-
-    let bookNav = document.createElement("div");
-    bookNav.setAttribute("class", "book-nav");
-    
-    let buttonTexts = ["Remove", "Favorite", "Read"];
-    buttonTexts.forEach(function(text) {   
-        let button = document.createElement("button");
-        button.textContent = text;
-        bookNav.appendChild(button);
-    })
-    bookCard.appendChild(bookNav);
-
-    return bookCard;
-}
-
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-}
-
-const bookGallery = document.querySelector(".book-gallery");
-
-function displayLibraryToBookGallery() {
-    for (let i = 0; i < myLibrary.length; i++)
-    {   
-        let bookCard = myLibrary.at(i).createHTMLBookCard();
-
-        bookGallery.appendChild(bookCard);
-    }
-}
-
-let catInTheHat = new Book("Cat in the Hat", "Dr. Seuss", 61, 0, false, false);
-addBookToLibrary(catInTheHat);
-displayLibraryToBookGallery();
-
 let morningColor = "#9bbed1";
 let afternoonColor = "#639bc9";
 let eveningColor = "#b55c04"; 
@@ -145,3 +31,130 @@ let day = time.getUTCDate();
 time.toLocaleDateString
 let year = time.getUTCFullYear();
 document.getElementById("date").textContent = `${month} ${day}, ${year}`;
+
+const myLibrary = [];
+
+function Book(title, author, amntOfPages, pagesRead, haveRead, favorite, coverSrcLink) {
+    this.title = title;
+    this.author = author;
+    this.pages = amntOfPages;
+    this.pagesRead = pagesRead;
+    this.haveRead = haveRead;
+    this.favorited = favorite;
+    this.coverLink = coverSrcLink;
+}
+
+Book.prototype.createHTMLBookCard = function() {
+    let bookCard = document.createElement("div");
+    bookCard.setAttribute("class", "book-card");
+
+    let bookCover = document.createElement("img");
+    bookCover.setAttribute("class", "book-cover");
+    if (this.coverLink != null) {
+        bookCover.setAttribute("src", this.coverLink);
+    }
+    else {
+        bookCover.setAttribute("src", "images/book_icon.svg");
+    }
+
+    bookCard.appendChild(bookCover);
+
+    let bookInfo = document.createElement("div");
+    bookInfo.setAttribute("class","book-info");
+
+    let bookTitle = document.createElement("h1");
+    bookTitle.setAttribute("class", "title");
+    bookTitle.textContent = this.title;
+    bookInfo.appendChild(bookTitle);
+
+    let bookAuthor = document.createElement("p");
+    bookAuthor.setAttribute("class", "author");
+    bookAuthor.textContent = this.author;
+    bookInfo.appendChild(bookAuthor);
+
+    let pagesSection = document.createElement("div");
+    pagesSection.setAttribute("class", "pages-section");
+
+    let pagesDisplay = document.createElement("div");
+    pagesDisplay.setAttribute("class", 'pages-display');
+
+    let bookPagesRead = document.createElement("p");
+    bookPagesRead.setAttribute("class", "pages-read");
+    bookPagesRead.textContent = this.pagesRead;
+    pagesDisplay.appendChild(bookPagesRead);
+
+    let division = document.createElement("p");
+    division.textContent = " / ";
+    pagesDisplay.appendChild(division);
+
+    let bookPages = document.createElement("p");
+    bookPages.setAttribute("class", "pages");
+    bookPages.textContent = this.pages;
+    pagesDisplay.appendChild(bookPages);
+
+    pagesSection.appendChild(pagesDisplay);
+
+    let subPageButton = document.createElement("button");
+    subPageButton.setAttribute("class", "page-button subtract");
+    subPageButton.textContent = "-";
+
+    subPageButton.addEventListener('click', () => {
+        if (this.pagesRead != 0)
+        {
+            this.pagesRead--;
+            bookPagesRead.textContent = this.pagesRead;
+        }
+    })
+
+    let addPageButton = document.createElement("button");
+    addPageButton.setAttribute("class", "page-button add");
+    addPageButton.textContent = "+";
+
+    addPageButton.addEventListener('click', () => {
+        if (this.pagesRead != this.pages)
+            {
+                this.pagesRead++;
+                bookPagesRead.textContent = this.pagesRead;
+            }
+    })
+
+    pagesSection.appendChild(subPageButton);
+    pagesSection.appendChild(addPageButton);
+
+    bookInfo.append(pagesSection);
+    bookCard.appendChild(bookInfo);
+
+    let bookNav = document.createElement("div");
+    bookNav.setAttribute("class", "book-nav");
+    
+    let buttonTexts = ["Remove", "Favorite", "Read"];
+    buttonTexts.forEach(function(text) {   
+        let button = document.createElement("button");
+        button.textContent = text;
+        bookNav.appendChild(button);
+    })
+    bookCard.appendChild(bookNav);
+
+    return bookCard;
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+const bookGallery = document.querySelector(".book-gallery");
+
+function displayLibraryToBookGallery() {
+    for (let i = 0; i < myLibrary.length; i++)
+    {   
+        let bookCard = myLibrary.at(i).createHTMLBookCard();
+
+        bookGallery.appendChild(bookCard);
+    }
+}
+
+let catInTheHat = new Book("Cat in the Hat", "Dr. Seuss", 61, 0, false, false, null);
+let scottPilgrim = new Book("Scott Pilgrim's Precious Little Life", "Bryan Lee O'Malley", 168, 0, false, true, "https://m.media-amazon.com/images/I/71iPLgUFlmL._SY342_.jpg");
+addBookToLibrary(catInTheHat);
+addBookToLibrary(scottPilgrim);
+displayLibraryToBookGallery();
